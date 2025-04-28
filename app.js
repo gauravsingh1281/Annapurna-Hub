745395ad8ae93b3167acc1d3d9c5e38c30fb5af1
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-29 02:21:24 +0530

    login and register page added.

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
 app.listen(port, () => {
   console.log(`App started listening on port ${port}`);
 });
